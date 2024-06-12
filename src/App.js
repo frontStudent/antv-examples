b095204112fff71e54cbd3c78cbd029f5d265bae
shiyubei <shiyubei@qince.com>
2024-06-12 17:31:31 +0800

    feat: xflow

--- src/App.js
+++ src/App.js
@@ -1,83 +1,20 @@
-import React, {useEffect, useRef} from 'react'
-import { Graph } from '@antv/x6'
-import './index.css'
+import { XFlow, XFlowGraph, Grid, Background, Snapline } from "@antv/xflow";
+import React from "react";
+import { InitNode } from "./InitNode";
+import "./index.css";
 
-const data = {
-  nodes: [
-    {
-      id: 'node1',
-      shape: 'rect',
-      x: 40,
-      y: 40,
-      width: 100,
-      height: 40,
-      label: 'hello',
-      attrs: {
-        // body 是选择器名称，选中的是 rect 元素
-        body: {
-          stroke: '#8f8f8f',
-          strokeWidth: 1,
-          fill: '#fff',
-          rx: 6,
-          ry: 6,
-        },
-      },
-    },
-    {
-      id: 'node2',
-      shape: 'rect',
-      x: 160,
-      y: 180,
-      width: 100,
-      height: 40,
-      label: 'world',
-      attrs: {
-        body: {
-          stroke: '#8f8f8f',
-          strokeWidth: 1,
-          fill: '#fff',
-          rx: 6,
-          ry: 6,
-        },
-      },
-    },
-  ],
-  edges: [
-    {
-      shape: 'edge',
-      source: 'node1',
-      target: 'node2',
-      label: 'x6',
-      attrs: {
-        // line 是选择器名称，选中的边的 path 元素
-        line: {
-          stroke: '#8f8f8f',
-          strokeWidth: 1,
-        },
-      },
-    },
-  ],
-}
-const Example = () => {
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const graph = new Graph({
-      container: containerRef.current,
-      background: {
-        color: "#F2F7FA",
-      },
-    });
-
-    graph.fromJSON(data); // Render elements
-    graph.centerContent(); // Center content
-  }, []);
-
+const Page = () => {
   return (
-    <div className="helloworld-app">
-      <div className="app-content" ref={containerRef} />
+    <div className="xflow-guide">
+      <XFlow>
+        <XFlowGraph zoomable minScale={0.5} />
+        <Grid type="dot" />
+        <Background color="#F2F7FA" />
+        <Snapline sharp />
+        <InitNode />
+      </XFlow>
     </div>
   );
 };
 
-export default Example;
+export default Page;
